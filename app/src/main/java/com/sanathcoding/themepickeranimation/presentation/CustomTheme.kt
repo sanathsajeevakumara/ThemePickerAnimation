47284c1f111d5ac7eb8e854e95158ffb36a736f1
Sanath <sanath@entrusttitle.com>
2023-06-21 11:37:38 +0530

    CirclePath animation is added

--- app/src/main/java/com/sanathcoding/themepickeranimation/presentation/CustomTheme.kt
+++ app/src/main/java/com/sanathcoding/themepickeranimation/presentation/CustomTheme.kt
@@ -13,20 +13,20 @@ data class CustomTheme(
 val darkTheme = CustomTheme(
     primaryColor = Color(0xFFE9B518),
     backgroundColor = Color(0xFF111111),
-    textColor = Color(0xffFFFFFF),
+    textColor = Color(0x88E8C660),
     image = R.drawable.dark,
 )
 
 val lightTheme = CustomTheme(
-    primaryColor = Color(0xFF2CB6DA),
+    primaryColor = Color(0xFFFFFFFF),
     backgroundColor = Color(0xFFF1F1F1),
-    textColor = Color(0xff000000),
+    textColor = Color(0xFF232526),
     image = R.drawable.light,
 )
 
 val pinkTheme = CustomTheme(
     primaryColor = Color(0xFFF01EE5),
     backgroundColor = Color(0xFF110910),
-    textColor = Color(0xFFEE8CE1),
+    textColor = Color(0x8FEE8CE1),
     image = R.drawable.pink,
 )
